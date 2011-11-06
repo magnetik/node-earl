27db118efedc1e83f08453d0447827adb239a2e9
Baptiste Lafontaine <baptiste33@gmail.com>
2011-11-06 22:32:28 +0100

    updating

--- test.js
+++ test.js
@@ -4,6 +4,3 @@ var e = new earl.Earl();
 
 e.setPrefix("wit", "http://www.w3.org/2005/Incubator/webid/earl/RelyingParty#");
 
-e.addAssertion(":assert1", ":certificate", "wit:certificateProvidedSAN", ":result1");
-
-e.writeFile("test.txt");
