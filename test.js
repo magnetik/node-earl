a82ef912104e0df5c88ab4bbee448c0777c78755
Baptiste Lafontaine <baptiste33@gmail.com>
2011-11-06 14:35:24 +0100

    Test is working, lib too

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ var earl = require('../node-earl');
 
 var e = new earl.Earl();
 
-e.setPrefix("wit", "http://www.w3.org/2005/Incubator/webid/earl/RelyingParty");
+e.setPrefix("wit", "http://www.w3.org/2005/Incubator/webid/earl/RelyingParty#");
 
-e.addAssertion(":assert1","", "");
+e.addAssertion(":assert1", ":certificate", "wit:certificateProvidedSAN", ":result1");
 
 e.writeFile("test.txt");
