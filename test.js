dff3d7fc0f51ec715c8514bd3679456a0b6ae1ae
Baptiste Lafontaine <baptiste33@gmail.com>
2011-11-09 08:50:48 +0100

    Keep refactoring. Creating assertion and result should work

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var earl = require('../node-earl');
 
-var e = new earl.Earl();
+var earlDocument = new earl.EarlDocument();
 
-e.setPrefix("wit", "http://www.w3.org/2005/Incubator/webid/earl/RelyingParty#");
+earlDocument.addAssertion(":assert1", ":certificate", "wit:certificateProvidedSAN");
 
+earlDocument.writeFile('test.txt');
