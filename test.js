c984dd96e829f53636a79ad61defcef6cd55a0c3
Baptiste Lafontaine <baptiste33@gmail.com>
2011-11-12 12:41:21 +0100

    Start documentation + fixed writeFile bug

--- test.js
+++ test.js
@@ -2,6 +2,11 @@ var earl = require('../node-earl');
 
 var earlDocument = new earl.EarlDocument();
 
+// add wit prefix
+earlDocument.setPrefix("wit", "http://purl.org/dc/terms/");
+
 earlDocument.addAssertion(":assert1", ":certificate", "wit:certificateProvidedSAN");
+
+earlDocument.getAssertion(":assert1").toRDF();
 
 earlDocument.writeFile('test.txt');
