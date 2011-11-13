dcd44de5fd01dbd64f1694f35c61c9501f52edd1
Baptiste Lafontaine <baptiste33@gmail.com>
2011-11-13 23:23:42 +0100

    Adding EarlResult support

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Node.js module to help producing EARL (http://www.w3.org/WAI/intro/earl) reports.
 
+Documentation is available here : http://magnetik.github.com/node-earl/doc/
 
 Any help is welcome !
--- test.js
+++ test.js
@@ -5,8 +5,8 @@ var earlDocument = new earl.EarlDocument();
 // add wit prefix
 earlDocument.setPrefix("wit", "http://purl.org/dc/terms/");
 
-earlDocument.addAssertion(":assert1", ":certificate", "wit:certificateProvidedSAN");
+earlDocument.addAssertion(":assert1", ":certificate", "wit:certificateProvidedSAN").toRDF();
 
-earlDocument.getAssertion(":assert1").toRDF();
+earlDocument.getAssertion(":assert1").addResult("earl:passed").toRDF();
 
 earlDocument.writeFile('test.txt');
